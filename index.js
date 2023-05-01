51e5d86b264c44b9efffe69d1cf6befc862d49b4
André Felipe <doro123andre@gmail.com>
2023-05-01 17:55:27 -0300

    atualizei o js

---
+++ index.js
@@ -0,0 +1,6 @@
+function update(){
+    let bill = Number(document.getElementById('yourBill').value)
+    let tipPercent = document.getElementById('tipInput').value
+    let splt = document.getElementById('splitInput').value
+    
+}
